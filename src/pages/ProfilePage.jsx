b9075515ababe5b9b5846ab7518d81bd46481658
Abhinaba <dasjayanti208@gmail.com>
2025-07-10 09:24:11 +0530

    .

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { Navbar } from "../components/navbar";
+import { Navbar } from "../components/Navbar";
 
 const ProfilePage = () => {
   const [products, setProducts] = useState([]);
